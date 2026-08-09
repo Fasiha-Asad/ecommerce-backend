81460cafc64e15e8256d49cbb6bf05fec5dc633b
Fasiha Asad <fasihaasad513@gmail.com>
2026-08-09 01:28:14 -0700

    Add get cart endpoint

--- Handlers/cart.py
+++ Handlers/cart.py
@@ -2,3 +2,10 @@ from fastapi import APIRouter
 from database.connection import conn,cursor
 from model.models import CartItemsCreate
 router=APIRouter()
+@router.get("/cart")
+def get_cart():
+    cursor.execute("""
+    SELECT * FROM carts
+    """)
+    cart = cursor.fetchall()
+    return cart
