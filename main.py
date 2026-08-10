c5f18295868b6a4762eb6d164c2f3c1c717aa9bd
Fasiha Asad <fasihaasad513@gmail.com>
2026-08-10 07:55:20 -0700

    add orders router

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from database.create_schema import create_schema
 from handlers.auth import router as auth_router
 from handlers.products import router as products_router
 from handlers.cart import router as cart_router
+from handlers.orders import router as orders_router
 
 
 app=FastAPI()
@@ -10,3 +11,4 @@ create_schema()
 app.include_router(auth_router)
 app.include_router(products_router)
 app.include_router(cart_router)
+app.include_router(orders_router)
