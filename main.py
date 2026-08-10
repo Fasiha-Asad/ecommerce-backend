de0f17851ec939156c521ac6d385c3ff7e5f3b1b
Fasiha Asad <fasihaasad513@gmail.com>
2026-08-10 11:30:55 -0700

    add users  router

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from handlers.auth import router as auth_router
 from handlers.products import router as products_router
 from handlers.cart import router as cart_router
 from handlers.orders import router as orders_router
+from handlers.users import router as users_router
 
 
 app=FastAPI()
@@ -12,3 +13,4 @@ app.include_router(auth_router)
 app.include_router(products_router)
 app.include_router(cart_router)
 app.include_router(orders_router)
+app.include_router(users_router
