934e3d437551bd737ea1c12aa03d85fb899598c5
Fasiha Asad <fasihaasad513@gmail.com>
2026-08-10 11:25:31 -0700

    add put  current user

--- Handlers/users.py
+++ Handlers/users.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter,Header
 from database.connection import conn,cursor
 from handlers.auth import verify_token
+from model.models import UserUpdate
 router=APIRouter()
 
 @router.get("/users/me")
 def get_current_user(authorization:str=Header()):
-    token=authorization.replace("Bearer","")
+    token=authorization.replace("Bearer ","")
     payload=verify_token(token)
     user_id=payload["user_id"]
 
@@ -13,7 +14,34 @@ def get_current_user(authorization:str=Header()):
     SELECT id, first_name, last_name, email, phone, created_at, updated_at
     FROM users
     WHERE id=?
-    """,(user_id))
+    """,(user_id,))
     user=cursor.fetchone()
     return user
+
+@router.put("/users/me")
+def upd_current_user(user:UserUpdate,authorization:str=Header()):
+    token=authorization.replace("Bearer ","")
+    payload=verify_token(token)
+    user_id=payload["user_id"]
+
+    cursor.execute("""
+    UPDATE users 
+    SET first_name=?,
+        last_name=?,
+        phone=?
+    where id=?
+    """,
+    (
+        user.first_name,
+        user.last_name,
+        user.phone,
+        user_id
+    ))
+    conn.commit()
+    return  {
+        "messages":"User updated successfully"
+    }
+
+
+
 
